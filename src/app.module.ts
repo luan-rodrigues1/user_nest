ceb0748e6feebe30bfeca5cec4ab5ef4a6d9cd42
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-11-19 17:20:40 -0300

    fix: add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,19 @@ import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users/entities/user.entity";
+import { ConfigModule } from "@nestjs/config";
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
         TypeOrmModule.forRoot({
             type: "mysql",
-            host: "localhost",
-            port: 3306,
-            username: "root",
-            password: "1234",
-            database: "user_nest",
+            host: process.env.DB_HOST!,
+            port: parseInt(process.env.DB_PORT!),
+            username: process.env.DB_USER!,
+            password: process.env.DB_PASSWORD!,
+            database: process.env.DB_DATABASE!,
             synchronize: true,
             logging: true,
             entities: [User],
