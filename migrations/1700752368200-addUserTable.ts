967845fb76f4edf65ffdf307edb5f6bf262a0f4c
luan-rodrigues1 <rodrigues.luan20@gmail.com>
2023-11-23 12:15:37 -0300

    fix: change migration file

--- migrations/1700752368200-addUserTable.ts
+++ migrations/1700752368200-addUserTable.ts
@@ -1,6 +1,7 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
-export class Teste1700686145395 implements MigrationInterface {
+export class AddUserTable1700752368200 implements MigrationInterface {
+
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(new Table({
             name: "users",
